146d44f234ff6abbb75a9b9e066c9010c4f2f841
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-01-17 23:57:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* script 태그를 제외한 그 안의 순수한 javascript 코드만 복사해온다 */
-/* 사실 강의에서는 script.js 말고 보기좋게 colors.js 라는 이름의 파일명으로 만들었다. */
+/* 사실 강의에서는 script.js 말고 보기좋게 colors.js 라는 이름의 파일명으로 만들었다. 
+이것은 내가 정리하기 편하려고 이렇게 지은것이고, 나중에 실제로 코드를 짜는 상황이 되면 강의처럼 이름을 생성하자.*/
 
 var Links = {
   setColor:function (color)
